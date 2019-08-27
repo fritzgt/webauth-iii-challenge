0689d6dc1171b1ecf5e08c2d03dc42e2b179c78c
fritzgt <tuanis@live.com>
2019-08-27 08:34:22 +0300

    implemented password hash for register

--- usersRouter/index.js
+++ usersRouter/index.js
@@ -1,6 +1,9 @@
 //import express library
 const express = require('express');
 
+//importing bcryptjs library
+const bcrypt = require('bcryptjs');
+
 //implement router
 const router = express.Router();
 
@@ -9,12 +12,17 @@ const db = require('../model/users-model');
 
 //creating a new user
 router.post('/register', async (req, res) => {
-  const newUser = req.body;
+  const credentials = req.body;
+  //creating hash using bcrypt
+  const hash = bcrypt.hashSync(credentials.password, 14);
+
+  credentials.password = hash;
+
   try {
-    const data = await db.add(newUser);
-    res.status(200).json({ data });
+    const data = await db.add(credentials);
+    res.status(200).json({ message: 'You are registered!' });
   } catch {
-    res.status(500).json({ message: 'Server side err' });
+    res.status(500).json({ message: 'Server side error' });
   }
 });
 
