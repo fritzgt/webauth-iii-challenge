b5cf1babbb4bb18414d514098e53c7336095d71c
fritzgt <tuanis@live.com>
2019-08-27 07:44:52 +0300

    working on model and end points

--- knexfile.js
+++ knexfile.js
@@ -4,40 +4,40 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './users.sqlite3'
+      filename: './auth.sqlite3'
     },
     useNullAsDefault: true
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user: 'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user: 'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 };
--- migrations/20190827071551_users.js
+++ migrations/20190827071551_users.js
@@ -1,5 +1,5 @@
 exports.up = function(knex) {
-  return knex.schema.createTable(users, tbl => {
+  return knex.schema.createTable('users', tbl => {
     //auto id
     tbl.increments();
     //username
