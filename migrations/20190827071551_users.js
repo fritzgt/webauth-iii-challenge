18b49186ba075e366ea506529f6ef6922611f614
fritzgt <tuanis@live.com>
2019-08-27 07:30:36 +0300

    creating table

--- migrations/20190827071551_users.js
+++ migrations/20190827071551_users.js
@@ -1,3 +1,20 @@
-exports.up = function(knex) {};
+exports.up = function(knex) {
+  return knex.schema.createTable(users, tbl => {
+    //auto id
+    tbl.increments();
+    //username
+    tbl
+      .text('username', 128)
+      .unique()
+      .notNullable();
+    //Password
+    tbl.text('password').notNullable();
+    //department field to group the users
+    tbl.text('department');
+  });
+};
 
-exports.down = function(knex) {};
+exports.down = function(knex) {
+  //dropping table
+  return knex.schema.dropTableIfExists('users');
+};
