492f7d4536b06d6ec5fa8f53c2856d8a0a8e8484
fritzgt <tuanis@live.com>
2019-08-27 07:16:33 +0300

    added the migration file

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,19 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './users.sqlite3'
+    },
+    useNullAsDefault: true
   },
 
   staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -29,7 +29,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -40,5 +40,4 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   }
-
 };
